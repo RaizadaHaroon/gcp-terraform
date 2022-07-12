81ea57fa4e74be43bbf6cb997dec64d842f094f0
raizadaharoon <haroon.raizada@cloudreach.com>
2022-07-12 18:09:45 +0100

    changes

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "gcs" {
     bucket = "cr-lab-hraizada-2906225331-tfstate"
-    }
+  }
 }
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project     = "cr-lab-hraizada-2906225331"
-  region      = var.region
-  }
+  project = "cr-lab-hraizada-2906225331"
+  region  = var.region
+}
 module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   depends_on   = [module.gke]
   project_id   = var.project_id
   location     = module.gke.location
@@ -39,35 +39,35 @@ module "gcp-network" {
 }
 
 module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  
+  source            = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id        = var.project_id
+  name              = "${var.cluster_name}-${var.env_name}"
+  regional          = true
+  region            = var.region
+  network           = module.gcp-network.network_name
+  subnetwork        = module.gcp-network.subnets_names[0]
+  ip_range_pods     = var.ip_range_pods_name
+  ip_range_services = var.ip_range_services_name
+
   node_pools = [
     {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = 1
-      max_count                 = 2
-      disk_size_gb              = 30
-service_account           = "terraform-account@cr-lab-hraizada-2906225331.iam.gserviceaccount.com"
+      name            = "node-pool"
+      machine_type    = "e2-medium"
+      node_locations  = "europe-west1-b,europe-west1-c,europe-west1-d"
+      min_count       = 1
+      max_count       = 2
+      disk_size_gb    = 30
+      service_account = "terraform-account@cr-lab-hraizada-2906225331.iam.gserviceaccount.com"
     },
   ]
-node_pools_labels = {
+  node_pools_labels = {
     all = {
       env = "test"
     }
     default     = {}
     preemptible = {}
   }
-node_pools_oauth_scopes = {
+  node_pools_oauth_scopes = {
     all = []
 
     default-node-pool = [
